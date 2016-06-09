b20662d13f2c64e274556b639befa854e3d3d5b7
Roland Talvar <talvar.roland@gmail.com>
2016-06-09 11:37:33 +0300

    Changed service path

--- src/main/java/com/rolandtalvar/controller/ServicesController.java
+++ src/main/java/com/rolandtalvar/controller/ServicesController.java
@@ -2,24 +2,21 @@ package com.rolandtalvar.controller;
 
 import com.rolandtalvar.model.Smartphone;
 import com.rolandtalvar.repository.SmartphoneRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.google.gson.Gson;
 
 
 @RestController
-@RequestMapping("/smartphone")
+@RequestMapping("/service")
 public class ServicesController {
 
     @Autowired
     SmartphoneRepository smartphoneRepository;
 
-    @RequestMapping(value = "/smartphoneservice", method = RequestMethod.GET, params = "id")
+    @RequestMapping(value = "/smartphone", method = RequestMethod.GET, params = "id")
     public Smartphone getDescription(@RequestParam("id") int id) {
         Smartphone smartphone = smartphoneRepository.findById(id);
         return smartphone;
